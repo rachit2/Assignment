467de9f9ba35b6aab3bd225372d03c5e45c884ab
rachit2 <rachitverma.001@gmail.com>
2017-08-17 00:18:28 +0530

    Add files via upload

---
+++ 12.java
@@ -0,0 +1,20 @@
+class Reentrant {  
+    public synchronized void m() {  
+    n();  
+    System.out.println("this is m() method");  
+    }  
+    public synchronized void n() {  
+    System.out.println("this is n() method");  
+    }  
+}  
+class ReentrantExample{  
+public static void main(String args[]){  
+final Reentrant re=new Reentrant();  
+  
+Thread t1=new Thread(){  
+public void run(){  
+re.m();//calling method of Reentrant class  
+}  
+};  
+t1.start();  
+}}  
---
+++ 6.java
@@ -0,0 +1,22 @@
+class TestJoinMethod1 extends Thread{  
+ public void run(){  
+  for(int i=1;i<=5;i++){  
+   try{  
+    Thread.sleep(500);  
+   }catch(Exception e){System.out.println(e);}  
+  System.out.println(i);  
+  }  
+ }  
+public static void main(String args[]){  
+ TestJoinMethod1 t1=new TestJoinMethod1();  
+ TestJoinMethod1 t2=new TestJoinMethod1();  
+ TestJoinMethod1 t3=new TestJoinMethod1();  
+ t1.start();  
+ try{  
+  t1.join();  //Waits for thread t1 to end. After t1 ends then t2 and t3 starts
+ }catch(Exception e){System.out.println(e);}  
+  
+ t2.start();  
+ t3.start();  
+ }  
+}  
