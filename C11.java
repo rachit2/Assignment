d89b2bb34ad4cebf0d576e5b195e9b88f3b033d4
rachit2 <rachitverma.001@gmail.com>
2017-09-06 19:02:04 +0530

    Add files via upload

---
+++ C11.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ArrayListLinkedListExample {
+    
+    public static void main(String[] args) { 
+ 
+           
+  
+        ArrayList<String> arrlistobj = new ArrayList<String>();
+        arrlistobj.add("1. Alive is awesome");
+        arrlistobj.add("2. Love yourself");
+        System.out.println("ArrayList object output :"+ arrlistobj);
+ 
+         
+ 
+        LinkedList llobj = new LinkedList();
+        llobj.add("1. Alive is  awesome");  
+        llobj.add("2. Love yourself"); 
+        System.out.println("LinkedList object output :"+llobj);   
+ 
+ }
+}
